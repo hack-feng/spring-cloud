b9b2adeb1c6b5e2063aeb67d329ae2383f97e2c6
hack-feng <1150640979@qq.com>
2019-07-26 13:42:58 +0800

    修改spring boot admin的bug，添加gateway，去掉zuul

---
+++ cloud-center/spring-cloud-gateway/gateway-master/src/main/java/com/maple/gateway/GatewayMasterApplication.java
@@ -0,0 +1,13 @@
+package com.maple.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMasterApplication.class, args);
+    }
+
+}
