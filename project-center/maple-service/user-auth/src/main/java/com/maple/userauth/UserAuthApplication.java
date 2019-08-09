0d5ffca8e7389de6db93d7d711eb7625b51845c5
yiling <******@i-lingnet.com>
2019-08-09 11:17:07 +0800

    增加退出操作

--- project-center/maple-service/user-auth/src/main/java/com/maple/userauth/UserAuthApplication.java
+++ project-center/maple-service/user-auth/src/main/java/com/maple/userauth/UserAuthApplication.java
@@ -1,18 +1,17 @@
 package com.maple.userauth;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
- * @author
+ * @author zhua
  */
 @EnableDiscoveryClient
 @SpringBootApplication
-public class MapleAuthApplication {
+public class UserAuthApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MapleAuthApplication.class, args);
+        SpringApplication.run(UserAuthApplication.class, args);
     }
 }
--- project-center/maple-service/user-auth/src/main/java/com/maple/userauth/endpoint/AuthTokenController.java
+++ project-center/maple-service/user-auth/src/main/java/com/maple/userauth/endpoint/AuthTokenController.java
@@ -1,8 +1,16 @@
 package com.maple.userauth.endpoint;
 
+import cn.hutool.core.util.StrUtil;
 import com.maple.common.core.constant.SecurityConstants;
+import com.maple.common.core.util.R;
 import lombok.AllArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2RefreshToken;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,7 +26,7 @@ import java.security.Principal;
 @RequestMapping("/token")
 public class AuthTokenController {
     private static final String PROJECT_OAUTH_ACCESS = SecurityConstants.PROJECT_PREFIX + SecurityConstants.OAUTH_PREFIX + "access:";
-
+    private final TokenStore tokenStore;
     private final RedisTemplate redisTemplate;
 
     @RequestMapping("/login")
@@ -32,4 +40,22 @@ public class AuthTokenController {
         return user;
     }
 
+    @GetMapping("logout")
+    public R logout(@RequestHeader(value = HttpHeaders.AUTHORIZATION, required = false) String authHeader) {
+        if (StrUtil.isBlank(authHeader)) {
+            return R.ok("退出失败，token 为空");
+        }
+
+        String tokenValue = authHeader.replace(OAuth2AccessToken.BEARER_TYPE, StrUtil.EMPTY).trim();
+        OAuth2AccessToken accessToken = tokenStore.readAccessToken(tokenValue);
+        if (accessToken == null || StrUtil.isBlank(accessToken.getValue())) {
+            return R.ok("退出失败，token 无效");
+        }
+        tokenStore.removeAccessToken(accessToken);
+
+        OAuth2RefreshToken refreshToken = accessToken.getRefreshToken();
+        tokenStore.removeRefreshToken(refreshToken);
+
+        return R.ok("退出成功");
+    }
 }
