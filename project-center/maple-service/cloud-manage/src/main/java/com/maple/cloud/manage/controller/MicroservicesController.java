5558f9f1f359603b6c4c00971bfa8fc577ffa161
hack-feng <1150640979@qq.com>
2019-08-12 15:36:31 +0800

    添加系统管理-->新增接口

--- project-center/maple-service/cloud-manage/src/main/java/com/maple/cloud/manage/controller/MicroservicesController.java
+++ project-center/maple-service/cloud-manage/src/main/java/com/maple/cloud/manage/controller/MicroservicesController.java
@@ -5,7 +5,10 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.maple.cloud.manage.service.IMicroservicesService;
 import com.maple.common.core.util.R;
 import com.maple.system.api.bean.Microservices;
+import com.maple.system.api.ro.GatewayDefineRo;
 import com.maple.system.api.ro.MicroservicesRo;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -34,6 +37,9 @@ public class MicroservicesController {
         return R.ok(result);
     }
 
+    @ApiOperation(value = "新增微服务", notes = "新增一个微服务，根据选择判断是否自动生成config配置文件")
+    @ApiImplicitParam(name = "microservicesRo", value = "需要新增微服务实体对象", required = true,
+            dataType = "MicroservicesRo", dataTypeClass = MicroservicesRo.class)
     @PostMapping
     public R add(@RequestBody MicroservicesRo microservicesRo){
         Microservices microservices = microservicesRo.toBean(Microservices.class);
