8329fbd59dd8e21ca9df072f93701ad3e37109e2
hack-feng <1150640979@qq.com>
2019-08-19 21:56:59 +0800

    基础信息维护

--- project-center/maple-service/cloud-manage/src/main/java/com/maple/cloud/manage/controller/InfoController.java
+++ project-center/maple-service/cloud-manage/src/main/java/com/maple/cloud/manage/controller/InfoController.java
@@ -1,18 +1,20 @@
 package com.maple.cloud.manage.controller;
 
 
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
- * 前端控制器
+ * mysql，redis，rabbitMQ信息维护-前端控制器
  * </p>
  *
  * @author maple
  * @since 2019-08-08
  */
+@Api(value = "mysql，redis，rabbitMQ信息维护")
 @RestController
 @RequestMapping("/info")
 public class InfoController {
